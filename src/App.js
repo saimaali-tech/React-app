ad5965bd246f13751441bce68a3aa2cc3e0934c7
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-18 12:26:09 +0500

    changed the project version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          This is a demo  project v1
+          This is a demo for docker project v1
         </p>
         <a
           className="App-link"
